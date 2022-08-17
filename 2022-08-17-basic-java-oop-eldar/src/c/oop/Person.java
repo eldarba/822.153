c59280b981d05d2544c947f463792b111d28ce0a
Ran <ranerlich@gmail.com>
2022-08-17 12:50:15 +0300

    AAA

--- 2022-08-17-basic-java-oop-eldar/src/c/oop/Person.java
+++ 2022-08-17-basic-java-oop-eldar/src/c/oop/Person.java
@@ -6,7 +6,20 @@ public class Person {
 	private int id;
 	private String name;
 	private int age;
+	
+	// constructor 1
+	public Person() {
+		
+	}
+	
+	// constructor 2
+	public Person(int id, String name, int age) {
+		setId(id);
+		setName(name);
+		setAge(age);
+	}
 
+	// methods
 	public void print() {
 		System.out.println("Person [id=" + id + ", name=" + name + ", age=" + age + "]");
 	}
---
+++ 2022-08-17-basic-java-oop-eldar/src/c/oop/PersonTest3.java
@@ -0,0 +1,14 @@
+package c.oop;
+
+public class PersonTest3 {
+
+	public static void main(String[] args) {
+		
+		Person p = new Person(101, "aaa", 28);
+		p.print();
+		
+		Person p2 = new Person();
+
+	}
+
+}
