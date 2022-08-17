413ef5d90ccd7f96b3bb57289fe98ced8a8b1b62
Ran <ranerlich@gmail.com>
2022-08-17 13:00:36 +0300

    AAA

--- 2022-08-17-basic-java-oop-eldar/src/c/oop/PersonTest3.java
+++ 2022-08-17-basic-java-oop-eldar/src/c/oop/PersonTest3.java
@@ -4,11 +4,15 @@ public class PersonTest3 {
 
 	public static void main(String[] args) {
 
-		Person p2 = new Person();
+		Person p1 = new Person();
 
-		Person p = new Person(101, "aaa", 28);
+		Person p2 = new Person(101, "aaa", 28);
 
 		Person p3 = new Person(102);
+
+		p1.print();
+		p2.print();
+		p3.print();
 
 	}
 
