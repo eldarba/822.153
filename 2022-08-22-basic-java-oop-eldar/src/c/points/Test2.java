04804ffb280fd52e3a4af22156acd4b1037c3a31
Ran <ranerlich@gmail.com>
2022-08-22 12:51:48 +0300

    AAA

---
+++ 2022-08-22-basic-java-oop-eldar/src/c/points/Test2.java
@@ -0,0 +1,15 @@
+package c.points;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		Point pt = new Point(5, 6);
+		Pixel px = new Pixel(3, 9, "Red");
+
+		pt.print();
+		px.print();
+
+	}
+
+}
