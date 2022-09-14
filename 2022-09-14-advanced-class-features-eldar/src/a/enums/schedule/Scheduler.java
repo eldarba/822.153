b8582a77eef9ac9533d8cd00eb40f323c1fecf1d
eyal <narnavg@gmail.com>
2022-09-14 13:00:26 +0300

    AAA

--- 2022-09-14-advanced-class-features-eldar/src/a/enums/schedule/Scheduler.java
+++ 2022-09-14-advanced-class-features-eldar/src/a/enums/schedule/Scheduler.java
@@ -9,22 +9,35 @@ public class Scheduler {
 	}
 
 	public void addTask(Day day, String tasks) {
-		if(day.tasks==null) {
-			day.tasks = tasks;
-		}else {
-			day.tasks += "\n" + tasks;
+		if (day.tasks == null) {
+			day.tasks = "  * " + tasks;
+		} else {
+			day.tasks += "\n" +  "  * " + tasks;
 		}
 	}
 
 	public String getTasks(Day day) {
 		// return day + ": " + (day.tasks != null ? day.tasks : "No Tasks Today");
 		String msg = day + ":\n";
-		if(day.tasks!=null) {
+		if (day.tasks != null) {
 			msg += day.tasks;
-		}else {
-			msg += "No Tasks Today";
+		} else {
+			msg += "  No Tasks Today";
 		}
 		return msg;
+	}
+
+	public void cancelTasks(Day day) {
+		day.tasks = null;
+		System.out.println("all tasks for " + day + " deleted");
+	}
+
+	public void displayAllWeekTasks() {
+		System.out.println("=== All Tasks ===================");
+		for (Day day : Day.values()) {
+			System.out.println(getTasks(day));
+		}
+		System.out.println("=== ========= ===================");
 	}
 
 }
--- 2022-09-14-advanced-class-features-eldar/src/a/enums/schedule/Test.java
+++ 2022-09-14-advanced-class-features-eldar/src/a/enums/schedule/Test.java
@@ -11,9 +11,14 @@ public class Test {
 		scheduler.addTask(Day.SUNDAY, "study Java");
 		scheduler.addTask(Day.MONDAY, "go hyking");
 		scheduler.addTask(Day.MONDAY, "go shopping");
+		scheduler.displayAllWeekTasks();
 		
+		System.out.println(scheduler.getTasks(Day.SUNDAY));
 		System.out.println(scheduler.getTasks(Day.MONDAY));
 		System.out.println(scheduler.getTasks(Day.TUESDAY));
+		
+		scheduler.cancelTasks(Day.MONDAY);
+		System.out.println(scheduler.getTasks(Day.MONDAY));
 
 	}
 
