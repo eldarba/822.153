779984b91cff1c013eb50d8507d26da473244af2
eyal <narnavg@gmail.com>
2022-10-24 11:53:53 +0300

    AAA

---
+++ 2022-10-24-jdbc/src/b/Test.java
@@ -0,0 +1,29 @@
+package b;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		try {
+			Scanner sc = new Scanner(System.in);
+			UserDao dao = new UserDaoDb();
+
+			User user = new User();
+			System.out.println("enter name");
+			user.setName(sc.nextLine());
+			System.out.println("enter email");
+			user.setEmail(sc.nextLine());
+			sc.close();
+
+			int id = dao.create(user);
+			System.out.println(user + " added to database");
+			System.out.println("id is: " + id);
+		} catch (UsersException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+	}
+
+}
