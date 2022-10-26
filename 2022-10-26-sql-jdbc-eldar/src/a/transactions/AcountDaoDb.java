4bce5573c9b16b4d6a9acd3d33e44391021573b7
eyal <narnavg@gmail.com>
2022-10-26 10:35:53 +0300

    26/10/2022

--- 2022-10-26-sql-jdbc-eldar/src/a/transactions/AcountDaoDb.java
+++ 2022-10-26-sql-jdbc-eldar/src/a/transactions/AcountDaoDb.java
@@ -1,4 +1,4 @@
-package d;
+package a.transactions;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- 2022-10-26-sql-jdbc-eldar/src/a/transactions/BusinessLogic.java
+++ 2022-10-26-sql-jdbc-eldar/src/a/transactions/BusinessLogic.java
@@ -1,4 +1,4 @@
-package d;
+package a.transactions;
 
 public class BusinessLogic {
 
