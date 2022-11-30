f38d99501b7580507abf694174a46dd00b2be489
jbt <jbt@host.docker.internal>
2022-11-30 11:33:47 +0200

    AAA

--- 2022-11-30-functional-programming/src/a/functional/interfaces/Demo6.java
+++ 2022-11-30-functional-programming/src/a/functional/interfaces/Demo6.java
@@ -1,9 +1,27 @@
 package a.functional.interfaces;
 
+import java.util.function.Consumer;
+
+import lombok.Data;
+
 public class Demo6 {
 
 	public static void main(String[] args) {
 
+		Consumer<Line> twice = line -> line.setLength(line.getLength() * 2);
+
+		Line line = new Line();
+		line.setLength(10);
+		System.out.println(line);
+
+		twice.accept(line);
+		System.out.println(line);
+
 	}
 
+}
+
+@Data
+class Line {
+	private int length;
 }
