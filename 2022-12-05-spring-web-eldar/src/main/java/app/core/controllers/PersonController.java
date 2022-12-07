2cb5ab7bba4870ba04b2991c70763ee3ced4fad5
jbt <jbt@host.docker.internal>
2022-12-07 10:18:07 +0200

    AAA

--- 2022-12-05-spring-web-eldar/src/main/java/app/core/controllers/PersonController.java
+++ 2022-12-05-spring-web-eldar/src/main/java/app/core/controllers/PersonController.java
@@ -1,16 +1,39 @@
 package app.core.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.core.entities.Person;
 import app.core.services.PersonService;
 
 @RestController
+@RequestMapping("/api")
 public class PersonController {
 
 	@Autowired
 	private PersonService personService;
 
+	// http://localhost:8080?id=1
+	@GetMapping
+	public Person findPerson(@RequestParam int id) {
+		return this.personService.getPerson(id);
+	}
+
+	// http://localhost:8080
+	@PostMapping
+	public void createPerson(@RequestBody Person person) {
+		person = this.personService.addPerson(person);
+		System.out.println(person);
+	}
+
+	// in a controller class we can map a method to a url
+	// http://localhost:8080/hello
+	@GetMapping("/hello")
 	public String hello() {
 		String result = personService.hello();
 		return result;
--- 2022-12-05-spring-web-eldar/src/main/java/app/core/services/PersonService.java
+++ 2022-12-05-spring-web-eldar/src/main/java/app/core/services/PersonService.java
@@ -3,6 +3,7 @@ package app.core.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import app.core.entities.Person;
 import app.core.repos.PersonRepo;
 import jakarta.transaction.Transactional;
 
@@ -15,6 +16,19 @@ public class PersonService {
 
 	public String hello() {
 		return "Hello";
+	}
+
+	public Person addPerson(Person person) {
+		if (!personRepo.existsById(person.getId())) {
+			person = personRepo.save(person);
+			return person;
+		} else {
+			throw new RuntimeException("addPerson failed - already exists");
+		}
+	}
+
+	public Person getPerson(int id) {
+		return this.personRepo.findById(id).orElseThrow(() -> new RuntimeException("person not found"));
 	}
 
 }
