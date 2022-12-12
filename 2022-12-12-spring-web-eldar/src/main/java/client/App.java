095ce8838b90b545e28bac27688af804c273b59b
narnavg <narnavg@gmail.com>
2022-12-12 17:11:25 +0200

    improved dictionary

--- 2022-12-12-spring-web-eldar/src/main/java/client/App.java
+++ 2022-12-12-spring-web-eldar/src/main/java/client/App.java
@@ -1,7 +1,6 @@
 package client;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,6 +9,8 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import org.springframework.web.client.RestTemplate;
+
+import lombok.Data;
 
 public class App {
 
@@ -22,16 +23,23 @@ public class App {
 		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fr.setLayout(null);
 
+		Font font = new Font(null, 0, 18);
 		JLabel lb = new JLabel("enter word: ");
+		lb.setFont(font);
 		JTextField tfCarNumber = new JTextField();
+		tfCarNumber.setFont(font);
 		JButton btGetCar = new JButton("Find");
+		btGetCar.setFont(font);
 		JTextArea ta = new JTextArea(10, 50);
+		ta.setFont(font);
 
-		lb.setBounds(10, 10, 150, 25);
-		tfCarNumber.setBounds(150, 10, 150, 25);
-		btGetCar.setBounds(10, 50, 150, 25);
+		lb.setBounds(10, 10, 150, 30);
+		tfCarNumber.setBounds(150, 10, 150, 30);
+		btGetCar.setBounds(10, 50, 150, 30);
 		ta.setBounds(10, 100, 500, 150);
 		ta.setEditable(false);
+		ta.setLineWrap(true);
+		ta.setWrapStyleWord(true);
 
 		fr.add(lb);
 		fr.add(tfCarNumber);
@@ -43,14 +51,28 @@ public class App {
 			System.out.println(word);
 			String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
 			System.out.println(url);
-			List<?> resp = rt.getForObject(url, ArrayList.class);
-			System.out.println(resp);
-			System.out.println(resp.getClass());
-//			ta.setText(resp);
+			Entry[] entries = rt.getForObject(url, Entry[].class);
+			ta.setText(entries[0].meanings[0].definitions[0].definition);
 		});
 
 		fr.setVisible(true);
 
+	}
+
+	@Data
+	static class Entry {
+		private String word;
+		private Meaning[] meanings;
+	}
+
+	@Data
+	static class Meaning {
+		Definition[] definitions;
+	}
+
+	@Data
+	static class Definition {
+		String definition;
 	}
 
 }
