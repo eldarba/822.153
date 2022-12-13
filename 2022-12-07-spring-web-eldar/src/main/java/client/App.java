bf15c21daa6e9a17e25ef0b9e03803a89412fa2a
Eldar <Eldar@DESKTOP-53V8189.lan>
2022-12-13 03:10:19 +0200

    AAA

--- 2022-12-07-spring-web-eldar/src/main/java/client/App.java
+++ 2022-12-07-spring-web-eldar/src/main/java/client/App.java
@@ -1,57 +1,112 @@
 package client;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Data;
 
 public class App {
 
 	public static void main(String[] args) {
 
 		RestTemplate rt = new RestTemplate();
+		ObjectMapper objectMapper = new ObjectMapper();
 
 		JFrame fr = new JFrame("Dictionary Application");
-		fr.setBounds(100, 100, 600, 300);
+		fr.setBounds(100, 100, 1200, 400);
 		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fr.setLayout(null);
 
-		JLabel lb = new JLabel("enter word: ");
-		JTextField tfCarNumber = new JTextField();
+		JLabel lbEnterWord = new JLabel("enter word: ");
+		JTextField tfEnterWord = new JTextField();
 		JButton btGetCar = new JButton("Find");
-		JTextArea ta = new JTextArea(10, 50);
+		JTextArea textArea = new JTextArea(0, 1500);
 
-		lb.setBounds(10, 10, 150, 25);
-		tfCarNumber.setBounds(150, 10, 150, 25);
-		btGetCar.setBounds(10, 50, 150, 25);
-		ta.setBounds(10, 100, 500, 150);
-		ta.setEditable(false);
+		lbEnterWord.setBounds(10, 10, 150, 25);
+		tfEnterWord.setBounds(100, 10, 150, 25);
+		btGetCar.setBounds(10, 50, 240, 25);
 
-		fr.add(lb);
-		fr.add(tfCarNumber);
+		textArea.setEditable(false);
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		scrollPane.setBounds(10, 100, 1000, 150);
+
+		fr.add(lbEnterWord);
+		fr.add(tfEnterWord);
 		fr.add(btGetCar);
-		fr.add(ta);
+		fr.add(scrollPane);
 
 		btGetCar.addActionListener(e -> {
-			String word = tfCarNumber.getText();
-			System.out.println(word);
+			String word = tfEnterWord.getText();
 			String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
-			System.out.println(url);
-			List<?> resp = rt.getForObject(url, ArrayList.class);
-			System.out.println(resp.get(0));
-			System.out.println(resp.getClass());
-			System.out.println(resp.get(0).getClass());
-//			ta.setText(resp);
+			try {
+				Entry[] entries = rt.getForObject(url, Entry[].class);
+
+				StringBuilder stringBuilder = new StringBuilder();
+				for (int i = 0; i < entries.length; i++) {
+					Entry entry = entries[i];
+					stringBuilder.append(i + 1).append(") ").append(entry.word).append("\n");
+					Meaning[] meanings = entry.meanings;
+					for (Meaning meaning : meanings) {
+						stringBuilder.append(" [").append(meaning.partOfSpeech).append("]");
+						Definition[] definitions = meaning.definitions;
+						for (Definition definition : definitions) {
+							stringBuilder.append("\n").append("   - ").append(definition.definition);
+						}
+						stringBuilder.append("\n");
+					}
+					stringBuilder.append("\n");
+				}
+				textArea.setText(stringBuilder.toString());
+			} catch (RestClientResponseException responseException) {
+				String errorMsg = responseException.getResponseBodyAsString();
+				try {
+					DictionaryError dictionaryError = objectMapper.readValue(errorMsg, DictionaryError.class);
+					String err = dictionaryError.title + "\n" + dictionaryError.message + "\n"
+							+ dictionaryError.resolution;
+					textArea.setText(err);
+				} catch (Exception e1) {
+					textArea.setText(errorMsg);
+				}
+			}
 		});
 
 		fr.setVisible(true);
 
+	}
+
+	@Data
+	static class Entry {
+		String word;
+		Meaning[] meanings;
+	}
+
+	@Data
+	static class Meaning {
+		String partOfSpeech;
+		Definition[] definitions;
+	}
+
+	@Data
+	static class Definition {
+		String definition;
+	}
+
+	@Data
+	static class DictionaryError {
+		String title;
+		String message;
+		String resolution;
 	}
 
 }
