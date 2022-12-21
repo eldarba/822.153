8d9b5feedaced553ff2121a8b8eb4c4c0e6c8a03
narnavg <narnavg@gmail.com>
2022-12-21 14:21:52 +0200

    AAA

--- 2022-12-21-html-forms/src/main/java/app/core/web/MyController.java
+++ 2022-12-21-html-forms/src/main/java/app/core/web/MyController.java
@@ -2,6 +2,7 @@ package app.core.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,14 +13,35 @@ public class MyController {
 	public enum AlphabetPhoneticChar {
 		ALPHA, BRAVO, CHARLIE, DELTA, ECHO, FOXTROT, GOLF, HOTEL, INDIA, JULIETT, KILO, LIMA, MIKE, NOVEMBER, OSCAR,
 		PAPA, QUEBEC, ROMEO, SIERRA, TANGO, UNIFORM, VICTOR, WHISKEY, X_RAY, YANKEE, ZULU;
+
+		public static AlphabetPhoneticChar getChar(Character character) {
+			character = Character.toUpperCase(character);
+			AlphabetPhoneticChar[] arr = AlphabetPhoneticChar.values();
+			int index = character - 'A';
+			return arr[index];
+		}
+
+		public static String convert(String text) {
+			String msg = "";
+			for (char c : text.toCharArray()) {
+				if (c == ' ') {
+					msg += " SPACE - ";
+				} else {
+					msg += getChar(c) + " - ";
+				}
+			}
+			return msg;
+		}
+	}
+
+	@GetMapping("/convert")
+	public String convert(String text) {
+		return AlphabetPhoneticChar.convert(text);
 	}
 
 	@GetMapping("/specified-char")
-	public AlphabetPhoneticChar getChar(Character character) {
-		character = Character.toUpperCase(character);
-		AlphabetPhoneticChar[] arr = AlphabetPhoneticChar.values();
-		int index = character - 'A';
-		return arr[index];
+	public AlphabetPhoneticChar getChar(@RequestParam("char") Character character) {
+		return AlphabetPhoneticChar.getChar(character);
 
 	}
 
