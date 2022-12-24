4f8845a5ff3226b654a8b369a28c0d76974045b0
Eldar <Eldar@DESKTOP-A354E4R>
2022-12-24 22:20:14 +0200

    AAA

--- 2022-12-21-html-forms/src/main/java/app/core/HtmlFormsApplication.java
+++ 2022-12-21-html-forms/src/main/java/app/core/HtmlFormsApplication.java
@@ -2,7 +2,7 @@ package app.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// ===
 @SpringBootApplication
 public class HtmlFormsApplication {
 
