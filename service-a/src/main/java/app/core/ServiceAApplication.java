c7be91d93df2b5d8d3240ba003a3c61f937af2a7
jbt <jbt@406-ins>
2023-03-22 15:44:50 +0200

    micro services hystrix

--- service-a/src/main/java/app/core/ServiceAApplication.java
+++ service-a/src/main/java/app/core/ServiceAApplication.java
@@ -3,6 +3,9 @@ package app.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +14,12 @@ public class ServiceAApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAApplication.class, args);
 	}
+	
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 }
+ 
--- service-a/src/main/java/app/core/web/ControllerA.java
+++ service-a/src/main/java/app/core/web/ControllerA.java
@@ -1,15 +1,44 @@
 package app.core.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ControllerA {
-	
+
+	@Autowired
+	private RestTemplate rt;
+
+//	@Autowired
+//	private DiscoveryClient discoveryClient;
+//	
+//	@Autowired
+//	private LoadBalancerClient loadBalancerClient;
+
 	// http://localhost:8001/service/a
 	@GetMapping("/service/a")
 	public String handleA() {
-		return "service a";
+//		String instanceId = "service-b";
+//		URI serviceUri = getServiceUriLB(instanceId);
+//		String requestPath = "/service/b";
+//		String url = serviceUri + requestPath;
+		String url = "http://service-b/service/b";
+		String responseOfServiceB = rt.getForObject(url, String.class);
+		return "service a: " + responseOfServiceB;
 	}
+
+//	// without load balancer we always return the same instance
+//	URI getServiceUri(String serviceId) {
+//		List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+//		return instances.get(0).getUri();
+//	}
+//	
+//	// with load balancer we can choose a different instance each time
+//	URI getServiceUriLB(String serviceId) {
+//		ServiceInstance serviceInstance = loadBalancerClient.choose(serviceId);
+//		return serviceInstance.getUri();
+//	}
 
 }
