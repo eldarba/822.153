dce2cb83533a091d54b14cd1d687b952fb1b4f4a
jbt <jbt@host.docker.internal>
2023-04-17 10:49:43 +0300

    AAA

--- java_2023_04_17_reflection/src/a/Demo2.java
+++ java_2023_04_17_reflection/src/a/Demo2.java
@@ -1,5 +1,6 @@
 package a;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -12,14 +13,29 @@ public class Demo2 {
 		Calculator calculator = new Calculator();
 		// calculator.selfDestruct(); // compile error - not visible
 
-		// use reflection to invoke selfDestruct
 		try {
+			// use reflection to invoke selfDestruct - private method
 			Method selfDestructMethod = calculator.getClass().getDeclaredMethod("selfDestruct");
 			selfDestructMethod.setAccessible(true);
 			System.out.println(selfDestructMethod);
 			selfDestructMethod.invoke(calculator);
+
+			// use reflection to access - private fields
+			Field resultField = calculator.getClass().getDeclaredField("result");
+			resultField.setAccessible(true);
+			System.out.println(resultField);
+
+			// get
+			double result = resultField.getDouble(calculator);
+			System.out.println(result);
+
+			// set
+			resultField.setDouble(calculator, 250);
+			result = resultField.getDouble(calculator);
+			System.out.println(result);
+
 		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException e) {
+				| InvocationTargetException | NoSuchFieldException e) {
 			e.printStackTrace();
 		}
 
---
+++ java_2023_04_17_reflection/src/a/Demo3.java
@@ -0,0 +1,27 @@
+package a;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import types.Calculator;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		
+		
+		try {
+			Class<Calculator> clazz = Calculator.class;
+			System.out.println(clazz);
+			Constructor<Calculator> ctor = clazz.getConstructor();
+			Calculator calculator = ctor.newInstance();
+			calculator.add(25);
+			calculator.mul(3);
+			System.out.println(calculator.getResult());
+		} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
